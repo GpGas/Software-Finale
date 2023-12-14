9a0f335f45cdfbad04078696f1987e388bd518d4
GpGas <76699402+GpGas@users.noreply.github.com>
2023-12-14 16:02:00 -0500

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+f = open("sample_AAPL.txt", "r")
+listItems = f.read().splitlines()
+
+appleprices=listItems
+for i in range(0, len(listItems)):
+ appleprices[i] = float(listItems[i])
+
+print ("The Mean of the prices is" , np.mean(listItems) ,"\n" , "The Standard deviaton of the prices is" , np.std(listItems))
+
+print("Rounded the mean is" , (round(np.mean(listItems))) ,"\n" ,"Rouned Standard deviation" , (round(np.std(listItems))))
