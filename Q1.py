a8dcbc104e9177414cbff3305eb943f6a019cd1a
GpGas <76699402+GpGas@users.noreply.github.com>
2023-12-19 10:02:40 -0500

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -7,6 +7,6 @@ appleprices=listItems
 for i in range(0, len(listItems)):
  appleprices[i] = float(listItems[i])
 
-print ("The Mean of the prices is" , np.mean(listItems) ,"\n" , "The Standard deviaton of the prices is" , np.std(listItems))
+print ("The Mean of the prices is" , np.mean(listItems) ,"\n" , "The Standard deviation of the prices is" , np.std(listItems))
 
-print("Rounded the mean is" , (round(np.mean(listItems))) ,"\n" ,"Rouned Standard deviation" , (round(np.std(listItems))))
+print("Rounded the mean is" , (round(np.mean(listItems))) ,"\n" ,"Rounded Standard deviation" , (round(np.std(listItems))))
